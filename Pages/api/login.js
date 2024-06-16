06b1f2a860fe9aed9e29fd6a3fc82289225033f1
shrasti-pro <gshrashti022@gmail.com>
2024-06-16 20:55:50 +0530

    Create login.js

---
+++ Pages/api/login.js
@@ -0,0 +1,15 @@
+import { serialize } from 'cookie'
+ 
+export default function handler(req, res) {
+  const sessionData = req.body
+  const encryptedSessionData = encrypt(sessionData)
+ 
+  const cookie = serialize('session', encryptedSessionData, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 60 * 60 * 24 * 7, // One week
+    path: '/',
+  })
+  res.setHeader('Set-Cookie', cookie)
+  res.status(200).json({ message: 'Successfully set cookie!' })
+}
